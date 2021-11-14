dc3560b0d4efbec8e48403b857b998107a5377bc
Adam Zdebel <adam.zdebel1997@gmail.com>
2021-11-14 16:59:14 +0100

    Login functionality

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { reducer } from 'shared/store'
 
 /* Create root reducer, containing all features of the application */
 const rootReducer = combineReducers({
-  ...reducer,
+  reducer,
 })
 
 const store = configureStore(
