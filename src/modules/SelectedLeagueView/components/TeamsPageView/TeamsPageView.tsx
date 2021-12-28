7727ada1ac92223afa4435781df61e3fc74c7b50
Adam Zdebel <adam.zdebel1997@gmail.com>
2021-12-28 19:44:26 +0100

    Add content window for teams view

--- src/modules/SelectedLeagueView/components/TeamsPageView/TeamsPageView.tsx
+++ src/modules/SelectedLeagueView/components/TeamsPageView/TeamsPageView.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import { Select, MenuItem, Typography } from '@mui/material'
+import {
+  ContentWindow,
+  ContentHeaderWrapper,
+} from 'modules/SelectedLeagueView/container/SelectedLeagueView.style'
 
 export interface ITeamsPageView {
   selectedView: string
   changeViewFn: (value: string) => void
 }
 
-const TeamsPageView: React.FC<ITeamsPageView> = () => {
-  return <div>asdfgs</div>
+const TeamsPageView: React.FC<ITeamsPageView> = ({
+  selectedView,
+  changeViewFn,
+}) => {
+  return (
+    <>
+      <ContentWindow>
+        <ContentHeaderWrapper>
+          <Typography variant="h4" component="h1">
+            League name
+          </Typography>
+          <Select
+            value={selectedView}
+            onChange={(event) => changeViewFn(event.target.value)}
+            variant="filled"
+            disableUnderline
+          >
+            <MenuItem value="table">Table</MenuItem>
+            <MenuItem value="teams">Teams</MenuItem>
+          </Select>
+        </ContentHeaderWrapper>
+      </ContentWindow>
+    </>
+  )
 }
 
 export default TeamsPageView
